09ed02fd21ec67820c43c3ab65be85092c1f4322
Jinfijizo23 <1009508@student.sae.edu.au>
2018-12-21 14:02:31 +1100

    Orbs Designs Progress
    Developed three Orb design and awaiting feedback.

--- Assets/Scripts/PsRibbon.cs
+++ Assets/Scripts/PsRibbon.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ribbon : MonoBehaviour {
+public class PsRibbon : MonoBehaviour {
 
     public SkyMaster MasterGameobject;
 
@@ -10,11 +10,11 @@ public class Ribbon : MonoBehaviour {
     {
         if (other.gameObject.tag == "Orb")
         {
-            MasterGameobject.changeTint(other.GetComponent<OrbProperties>().Tintset);
+            MasterGameobject.changeTint(other.GetComponent<Psorbproperties>().Tintset);
             //Debug.Log(other.GetComponent<OrbProperties>().Tintset);
-            MasterGameobject.changeFloor(other.GetComponent<OrbProperties>().Groundset);
+            MasterGameobject.changeFloor(other.GetComponent<Psorbproperties>().Groundset);
             //Debug.Log(other.GetComponent<OrbProperties>().Groundset);
-            MasterGameobject.changeExpo(other.GetComponent<OrbProperties>().Expo);
+            MasterGameobject.changeExpo(other.GetComponent<Psorbproperties>().Expo);
             //Debug.Log(other.GetComponent<OrbProperties>().Expo);
         }
     }
--- Assets/Scripts/Psorbproperties.cs
+++ Assets/Scripts/Psorbproperties.cs
@@ -2,19 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OrbProperties : MonoBehaviour {
+public class Psorbproperties : MonoBehaviour {
 
     public Color Tintset;
     public Color Groundset;
     public float Expo;
-    private Renderer X;
-    public ParticleSystem y;
+    private ParticleSystem X;
+    private Renderer y;
     public float revive;
     public float settime = 5f;
 
     void Start()
     {
-        X = GetComponent<Renderer>();
+        X = GetComponent<ParticleSystem>();
+        y = GetComponent<Renderer>();
         revive = settime;
     }
     void Update()
@@ -30,14 +31,16 @@ public class OrbProperties : MonoBehaviour {
     {
         if (other.gameObject.tag == "Wisp")
         {
-            X.enabled = false;
-            y.Stop();
+            Debug.Log("Hit");
+            y.enabled = false;
+            //X.Stop();
         }
     }
     void Revive()
     {
-        X.enabled = true;
-        y.Play();
+        //X.Play();
+        y.enabled = true;
+        Debug.Log("Return");
         revive = settime;
     }
 }
